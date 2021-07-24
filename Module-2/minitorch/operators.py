1e799853cd20bfcb977d53ff64bc9b5545dc0ee5
jesusmiguel123 <45299666+jesusmiguel123@users.noreply.github.com>
2021-07-23 23:07:04 -0500

    Add files via upload

---
+++ Module-2/minitorch/operators.py
@@ -0,0 +1,198 @@
+import math
+
+
+def mul(x, y):
+    ":math:`f(x, y) = x * y`"
+    return x*y
+
+
+def id(x):
+    ":math:`f(x) = x`"
+    return x
+
+
+def add(x, y):
+    ":math:`f(x, y) = x + y`"
+    return x + y
+
+
+def neg(x):
+    ":math:`f(x) = -x`"
+    return -x
+
+
+def lt(x, y):
+    ":math:`f(x) =` 1.0 if x is less than y else 0.0"
+    if x < y:
+        return 1.0
+    else:
+        return 0.0
+
+
+def eq(x, y):
+    ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
+    if x == y:
+        return 1.0
+    else:
+        return 0.0
+
+
+def max(x, y):
+    ":math:`f(x) =` x if x is greater than y else y"
+    if x > y:
+        return x
+    else:
+        return y
+
+
+def sigmoid(x):
+    r"""
+    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}`
+
+    (See `<https://en.wikipedia.org/wiki/Sigmoid_function>`_ .)
+
+    Calculate as
+
+    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}` if x >=0 else :math:`\frac{e^x}{(1.0 + e^{x})}`
+
+    for stability.
+
+    """
+    return 1.0/(1.0 + math.exp(-x))
+
+
+def relu(x):
+    """
+    :math:`f(x) =` x if x is greater than 0, else 0
+
+    (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
+    """
+    if x > 0.0:
+        return x
+    else:
+        return 0.0
+
+
+def relu_back(x, y):
+    ":math:`f(x) =` y if x is greater than 0 else 0"
+    if x > 0.0:
+        return y
+    else:
+        return 0.0
+
+
+EPS = 1e-6
+
+
+def log(x):
+    ":math:`f(x) = log(x)`"
+    return math.log(x + EPS)
+
+
+def exp(x):
+    ":math:`f(x) = e^{x}`"
+    return math.exp(x)
+
+
+def log_back(a, b):
+    return b / (a + EPS)
+
+
+def inv(x):
+    ":math:`f(x) = 1/x`"
+    return 1.0 / x
+
+
+def inv_back(a, b):
+    return -(1.0 / a ** 2) * b
+
+
+def map(fn):
+    """
+    Higher-order map.
+
+    .. image:: figs/Ops/maplist.png
+
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+
+    Args:
+        fn (one-arg function): process one value
+
+    Returns:
+        function : a function that takes a list and applies `fn` to each element
+    """
+    def fun(ls):
+        for i in range(len(ls)):
+            ls[i] = fn(ls[i])
+        return ls
+    return fun
+
+def negList(ls):
+    "Use :func:`map` and :func:`neg` to negate each element in `ls`"
+    return map(neg)(ls)
+
+
+def zipWith(fn):
+    """
+    Higher-order zipwith (or map2).
+
+    .. image:: figs/Ops/ziplist.png
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+
+    Args:
+        fn (two-arg function): combine two values
+
+    Returns:
+        function : takes two equally sized lists `ls1` and `ls2`, produce a new list by
+        applying fn(x, y) one each pair of elements.
+
+    """
+    def fun(ls1, ls2):
+        ls3 = []
+        for i in range(len(ls1)):
+            ls3.append(fn(ls1[i], ls2[i]))
+        return ls3
+    return fun
+
+def addLists(ls1, ls2):
+    "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
+    return zipWith(add)(ls1, ls2)
+
+
+def reduce(fn, start):
+    """
+    Higher-order reduce.
+
+    .. image:: figs/Ops/reducelist.png
+
+
+    Args:
+        fn (two-arg function): combine two values
+        start (float): start value :math:`x_0`
+
+    Returns:
+        function : function that takes a list `ls` of elements
+        :math:`x_1 \ldots x_n` and computes the reduction :math:`fn(x_3, fn(x_2,
+        fn(x_1, x_0)))`
+
+    """
+    def fun(ls):
+        valor = start
+        for i in range(len(ls)):
+            valor = fn(valor, ls[i])
+        return valor
+    return fun
+
+def sum(ls):
+    """
+    Sum up a list using :func:`reduce` and :func:`add`.
+    """
+    return reduce(add, 0)(ls)
+
+def prod(ls):
+    """
+    Product of a list using :func:`reduce` and :func:`mul`.
+    """
+    return reduce(mul, 1)(ls)
